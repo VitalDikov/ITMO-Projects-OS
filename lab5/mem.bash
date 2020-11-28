345f1207d9395c20a714ea0f44574c6c5ae98825
root <root@localhost.localdomain>
2020-11-29 01:42:44 +0300

    init

---
+++ lab5/mem.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+declare -a arr
+cap=0
+rm report.log
+while [[ true ]]; do
+	arr+=(1 2 3 4 5 6 7 8 9 10)
+	let cap++
+	if [[ $cap == 10000 ]];	then 
+		cap=0
+		echo ${#arr[@]} >> report.log
+	fi
+done
