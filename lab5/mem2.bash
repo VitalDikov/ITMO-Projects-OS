321892f981e88e5bb531642fbd3c65cbb7f80d42
root <root@localhost.localdomain>
2020-11-29 06:57:31 +0300

    init 2-nd phase

---
+++ lab5/mem2.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+declare -a arr
+cap=0
+rm report2.log
+while [[ true ]]; do
+	arr+=(1 2 3 4 5)
+	let cap++
+	if [[ $cap == 10000 ]];	then 
+		cap=0
+		echo ${#arr[@]} >> report2.log
+	fi
+done
